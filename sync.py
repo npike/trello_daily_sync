eb236f4c800a03243c82844d3cd89a8067c40f20
Nicholas Pike <mavr1k@gmail.com>
2015-07-02 10:51:35 -0700

    updated sync to support board configurations that define day of week specific cards/tasks.

--- sync.py
+++ sync.py
@@ -3,16 +3,19 @@ import os
 import sys 
 sys.path.append(os.path.abspath(os.path.split(sys.argv[0])[0])+"/py-trello-master")
 sys.path.append(os.path.abspath(os.path.split(sys.argv[0])[0])+"/python-dateutil-1.5")
-print sys.path
 import os
 from trello import TrelloClient, Unauthorized, ResourceUnavailable
 from pprint import pprint
 import ConfigParser
+import calendar
+from datetime import date
 
 config = ConfigParser.RawConfigParser()
 config.read(os.path.abspath(os.path.split(sys.argv[0])[0])+'/config.properties')
 
-# setup: pip install requests_oauthlib
+# today is a [day of week]
+dayOfWeek = calendar.day_name[date.today().weekday()]
+print "Starting sync for " + dayOfWeek
 
 client = TrelloClient(
 	api_key=config.get('Trello', 'trello.api_key'), 
@@ -30,7 +33,11 @@ for board in client.list_boards():
 		if aList.name == "Daily":
 			print "[found daily list] " + aList.name, aList.board.name
 			# board dailie tasks are stored in [boardName.toLower].txt
-			dailyListIn  = open(os.path.abspath(os.path.split(sys.argv[0])[0])+'/'+aList.board.name.lower()+".txt", "rb")
+			boardConfigFile = os.path.abspath(os.path.split(sys.argv[0])[0])+'/'+aList.board.name.lower()+".txt"
+			if not os.path.exists(boardConfigFile):
+				print "No configuration file found for "+aList.board.name +", skipping."
+				continue
+			dailyListIn  = open(boardConfigFile, "rb")
 			# archive all existing cards in this list FIRST
 			print "Archiving old daily cards"
 			aList.archive_all_cards()
@@ -38,23 +45,39 @@ for board in client.list_boards():
 			# each line of dailyListIn represends a new card.
 			# if the line starts with a -, the card has a checklist and this is a list item
 
+			shouldAddCard = True
 			dailyCard = None
 			dailyCardChecklist = []
+			
 			for line in dailyListIn:
-				if not line.startswith("-"):
-					# check to make sure we werent processing a list
-					if dailyCardChecklist:
-						print "Added checklist for " + dailyCard.name
-						checklist = dailyCard.add_checklist("Checklist",
-                                       dailyCardChecklist)
-						dailyCardChecklist = []
-					# create a new card
-					print "Creating card for "+line
-					dailyCard = aList.add_card(line)
-				if line.startswith("-"):
-					# track all of the list items for this card
-					# we will add them all to the card later
-					dailyCardChecklist.append(line[1:].strip())
+
+				# skip empty lines
+				if not line.strip():
+					continue
+
+				# process sections as cards for specific days of the week
+				if line.startswith("["):
+					if "["+dayOfWeek+"]" in line:
+						print "Found section specific for this day of the week "+dayOfWeek
+						shouldAddCard = True
+					else:
+						shouldAddCard = False
+					continue
+
+				if shouldAddCard:
+					if not line.startswith("-"):
+						# check to make sure we werent processing a list
+						if dailyCardChecklist:
+							print "Added checklist for " + dailyCard.name
+							checklist = dailyCard.add_checklist("Checklist",dailyCardChecklist)
+							dailyCardChecklist = []
+						# create a new card
+						print "Creating card for "+line
+						dailyCard = aList.add_card(line)
+					elif line.startswith("-"):
+						# track all of the list items for this card
+						# we will add them all to the card later
+						dailyCardChecklist.append(line[1:].strip())
 
 			# clean up.  we might have a pending dailyCardChecklist
 			if dailyCardChecklist:
